323cb90e30fa5a7bed3300129d58dcb21aa8f273
kev07_ok <kevinarozamena10@gmail.com>
2026-09-08 23:35:29 -0300

    cambio grande

---
+++ header.js
@@ -0,0 +1,10 @@
+document.write(`
+  <header>
+    <h2>RIQSIN</h2>
+    <nav>
+      <a href="/">Inicio</a>
+      <a href="/metodo/">Método</a>
+      <a href="/investigaciones/">Investigación</a>
+    </nav>
+  </header>
+`);
